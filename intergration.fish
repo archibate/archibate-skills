9ae47f619af3de80ed16098a85fc27411496b7a4
archibate <1931127624@qq.com>
2026-03-16 13:56:10 +0800

    add claude-chill

--- intergration.fish
+++ intergration.fish
@@ -4,6 +4,15 @@ function claude
     $CLAUDE_TOOLS_DIR/claude-router.py $argv
 end
 
+if command -sq claude-chill
+    function claude-chill
+        set -l CLAUDE_TOOLS_DIR (path dirname (status --filename))
+        set -lx AGENT_BROWSER_SESSION (basename $PWD)-(command -sq openssl; and openssl rand -hex 8; or random)
+        # cargo install --git https://github.com/davidbeesley/claude-chill
+        command claude-chill -- $CLAUDE_TOOLS_DIR/claude-router.py $argv
+    end
+end
+
 if not command -sq commit
     function commit
         timeout -v -s INT 80s claude -p --model haiku --max-turns 50 "Make a git commit with commit message briefly describing what changed in the codebase. Stage and commit all changed files (including untracked ones). If some stagable files looks like should appear in .gitignore, add the file name pattern to .gitignore before stage. Do not edit files in this conversation."
