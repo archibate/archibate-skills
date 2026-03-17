467ccbc155b9a94c3487f033eba606af1cb9d5b9
archibate <1931127624@qq.com>
2026-03-17 10:00:30 +0800

    fix installer

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/bash
 
+set -e
+
 cd $(dirname $0)
 
-if test -d ~/.claude/; then
+if test -d ~/.claude; then
     echo -- Installing for Claude Code
-    ln -sf $PWD/skills ~/.claude/
-    ln -sf $PWD/CLAUDE.md ~/.claude/
+    mkdir -p ~/.claude/skills
+    ln -sf $PWD/skills/* ~/.claude/skills
+    ln -sf $PWD/CLAUDE.md ~/.claude/CLAUDE.md
 fi
 
-if test -d ~/.codex/; then
+if test -d ~/.codex; then
     echo -- Installing for Codex
-    ln -sf $PWD/skills ~/.codex/
+    mkdir -p ~/.codex/skills
+    rm -f ~/.codex/skills/archibate
+    ln -sf $PWD/skills ~/.codex/skills/archibate
     ln -sf $PWD/CLAUDE.md ~/.codex/AGENTS.md
 fi
